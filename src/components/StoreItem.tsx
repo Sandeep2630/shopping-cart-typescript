cca78f21c391cd37dcedeadc7917089632ce1cb9
sandeep2630 <katwal.sandeep28@gmail.com>
2023-01-12 16:28:28 +0530

    added increase decrease functionality

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Button, Card } from 'react-bootstrap';
+import { useShoppingCart } from '../context/shoppingCartContext';
 import { formatCurrency } from '../utilities/formatCurrency';
 
 type StoreItemProps = {
@@ -8,7 +9,9 @@ type StoreItemProps = {
     imgUrl:string
 }
 export  function StoreItem({id , name , price , imgUrl}:StoreItemProps) {
-    const quantity = 1;
+
+  const { getItemQuantity ,increaseCartQuantity ,decreaseCartQuantity , removeFromCart} = useShoppingCart()
+    const quantity = getItemQuantity(id);
   return (
     <>
     <Card className='h-100'>
@@ -19,16 +22,16 @@ export  function StoreItem({id , name , price , imgUrl}:StoreItemProps) {
             <span className='ms-2 text-muted'>{formatCurrency(price)}</span>
         </Card.Title>
         <div className='mt-auto'>
-            {quantity === 0 ? (<Button className='w-100'>+ Add to cart</Button>):(<div className='d-flex align-items-center flex-column ' style={{gap:'.5rem'}}>
+            {quantity === 0 ? (<Button onClick={()=>increaseCartQuantity(id)} className='w-100'>+ Add to cart</Button>):(<div className='d-flex align-items-center flex-column ' style={{gap:'.5rem'}}>
                 <div className='d-flex align-items-center justify-content-center' style={{gap:'.5rem'}}>
-                     <Button>-</Button>
+                     <Button onClick={()=>decreaseCartQuantity(id)}>-</Button>
                      <div>
                      <span className='fs-3'>{quantity}</span>
                         in cart
                      </div>
-                     <Button>+</Button>
+                     <Button onClick={()=>increaseCartQuantity(id)} >+</Button>
                 </div>
-                <Button variant='danger' size='sm'>Remove</Button>
+                <Button onClick={()=>removeFromCart(id)} variant='danger' size='sm'>Remove</Button>
             </div>)}
         </div>
       </Card.Body>
--- src/context/shoppingCartContext.tsx
+++ src/context/shoppingCartContext.tsx
@@ -35,16 +35,45 @@ export function ShoppingCartProvider ({children}:ShoppingCartProviderProps){
  
 
      function increaseCartQuantity(id:number){
+        setCartItems(currItems => {
+            if(currItems.find( item=>item.id===id)==null){
+
+                return [...currItems , {id , quantity:1}]
+            }else{
+                return currItems.map(item=>{
+                    if(item.id===id){
+                        return {...item , quantity:item.quantity + 1}
+                    }else{
+                        return item
+                    }
+                })
+            }
+        })
      }
-
-
 
      function decreaseCartQuantity(id:number){
+        setCartItems(currItems => {
+            if(currItems.find( item=>item.id===id)?.quantity==1){
 
+                return currItems.filter(item=>item.id!==id)
+            }else{
+                return currItems.map(item=>{
+                    if(item.id===id){
+                        return {...item , quantity:item.quantity - 1}
+                    }else{
+                        return item
+                    }
+                })
+            }
+        })
      }
 
+    
 
      function removeFromCart(id:number){
+        setCartItems((curritem)=>{
+            return curritem.filter(item=>item.id !== id) 
+        })
 
      }
 
