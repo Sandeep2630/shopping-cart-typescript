72f80e0bad583dd06c3175141597d599aa31c8c3
sandeep2630 <katwal.sandeep28@gmail.com>
2023-01-12 19:05:00 +0530

    added pages name

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -6,6 +6,7 @@ import storeItems from '../data/item.json'
 export function Store(){
     return (
         <>
+        <h1>Store Page</h1>
         <Row md={2} xs={1} lg={3} className='g-3'>
             {storeItems.map((item)=>{
                 return (
