0da04273089d4b767d242520e330a0a87f1d0470
sandeep2630 <katwal.sandeep28@gmail.com>
2023-01-12 17:00:57 +0530

    store change

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -15,23 +15,33 @@ export  function StoreItem({id , name , price , imgUrl}:StoreItemProps) {
   return (
     <>
     <Card className='h-100'>
+
         <Card.Img variant='top' src={imgUrl} height='200px' style={{objectFit:"cover"}} />
+        
       <Card.Body className='d-flex flex-column'>
+
         <Card.Title className='d-flex justify-content-between align-items-baseline mb-4'>
             <span className='fs-2'>{name}</span>
             <span className='ms-2 text-muted'>{formatCurrency(price)}</span>
         </Card.Title>
+
         <div className='mt-auto'>
             {quantity === 0 ? (<Button onClick={()=>increaseCartQuantity(id)} className='w-100'>+ Add to cart</Button>):(<div className='d-flex align-items-center flex-column ' style={{gap:'.5rem'}}>
+
                 <div className='d-flex align-items-center justify-content-center' style={{gap:'.5rem'}}>
+                  
                      <Button onClick={()=>decreaseCartQuantity(id)}>-</Button>
                      <div>
-                     <span className='fs-3'>{quantity}</span>
+
+                      <span className='fs-3'>{quantity}</span>
                         in cart
                      </div>
+
                      <Button onClick={()=>increaseCartQuantity(id)} >+</Button>
                 </div>
+
                 <Button onClick={()=>removeFromCart(id)} variant='danger' size='sm'>Remove</Button>
+                
             </div>)}
         </div>
       </Card.Body>
