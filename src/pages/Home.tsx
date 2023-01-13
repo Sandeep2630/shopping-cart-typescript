013376c1986f7b8b4e225c122bad2ae2a18e002f
sandeep2630 <katwal.sandeep28@gmail.com>
2023-01-13 18:43:58 +0530

    hi

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,3 @@
 export function Home (){
-    return <h1>Home Page still on the way</h1>
+    return <h1>Home Page</h1>
 }
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -6,7 +6,7 @@ import storeItems from '../data/item.json'
 export function Store(){
     return (
         <>
-        <h1>Store item list  Page</h1>
+        <h1>Store item</h1>
         <Row md={2} xs={1} lg={3} className='g-3'>
             {storeItems.map((item)=>{
                 return (
